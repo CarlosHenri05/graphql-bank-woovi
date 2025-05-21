71e0175374bf629985953dbc7492701682982278
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-05-21 17:07:33 -0300

    Adding Mutation, Query and AccountInput typeDefs

--- src/modules/schema/typeDefs.ts
+++ src/modules/schema/typeDefs.ts
@@ -1,14 +1,24 @@
 import { gql } from 'apollo-server'
 
-
 export const typeDefs = gql`
   type Account {
-  id: ID!
-  username: String!
-  amount: Float!
+    id: ID!
+    username: String!
+    balance: Float!
   }
-  
+
   type Query {
-  
+    account(id: ID!): Account
+    accounts: [Account!]!
   }
-  `
+
+  input CreateAccountInput {
+    username: String!
+    balance: Float!
+  }
+
+  type Mutation {
+    createAccount(input: CreateAccountInput!): Account!
+    deleteAccount(id: ID!): Boolean!
+  }
+`
